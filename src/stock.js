0c7645b99e4fc3534f686680c170651c7da97b1f
matrabinovich <mat.rabinovich@gmail.com>
2022-10-31 20:45:35 -0300

    primer comit

---
+++ src/stock.js
@@ -0,0 +1,84 @@
+const productos =
+[
+    {
+        id: 1,
+        nombre: "Buda",
+        tipo: "vela",
+        desc: "Vela aromatica",
+        precio: 1300,
+        img: './public/images/buda.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 2,
+        nombre: "Clasica",
+        tipo: "Vela",
+        desc: "Vela aromatica",
+        precio: 500,
+        img: 'public/images/clasica.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 3,
+        nombre: "Cuenco",
+        tipo: "Vela",
+        desc: "Vela aromatica",
+        precio: 900,
+        img: 'public/images/cuenco.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 4,
+        nombre: "Moderna",
+        tipo: "Vela",
+        desc: "Vela aromatica",
+        precio: 1600,
+        img: 'public/images/moderna.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 5,
+        nombre: "Moderna transparente",
+        tipo: "Vela",
+        desc: "Vela aromatica",
+        precio: 1200,
+        img: 'public/images/modernatrans.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 6,
+        nombre: "princess",
+        tipo: "vela",
+        desc: "Vela aromatica",
+        precio: 500,
+        img: 'public/images/princess.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 7,
+        nombre: "Vaso",
+        tipo: "vela",
+        desc: "Vela aromatica",
+        precio: 500,
+        img: 'public/images/vaso.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 8,
+        nombre: "Vintage",
+        tipo: "vela",
+        desc: "Vela aromatica",
+        precio: 500,
+        img: 'public/images/vintage.jpg',
+        cantidad:1
+    }
+];
+
+export { productos };
